3026ba689b85bd0a16c57d1f5c197497c11f3a99
Junpeng <j338c678@gmail.com>
2018-10-21 18:28:29 -0500

    junpeng and xinyun fiexed Question.cs and TicTacToe.cs

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -5,21 +5,24 @@ using UnityEngine.UI;
 
 public class Question : MonoBehaviour
 {
-
+    //create a new text for totalpoint
     public Text totalpoint;
-    public static float GPA;
+    //point for each question
     public static int point;
     public static int point1;
     public static int point2;
     public static int point3;
     public static int point4;
     public static int totalpoint1;
+    public static float GPA;
+
+
+    //element in each dropdown
     List<string> questions = new List<string>() { "A is member of object B", "B is member of Object A", "Product of A and B", "None of these" };
     List<string> questions1 = new List<string>() { "Operator-&", "Operator-||", "Operator-&&", "Operator +" };
     List<string> questions2 = new List<string>() { "A Slash (/)", "A Fullstop(.)", "A Comma (,)", "A Semicolon (;)" };
     List<string> questions3 = new List<string>() { "<fiostream>", "<ifstream>", "<ofstream>", "<fstream>" };
     List<string> questions4 = new List<string>() { "thrown", "threw", "throw", "throws" };
-    
     public Dropdown dropdown;
     public Dropdown dropdown1;
     public Dropdown dropdown2;
@@ -35,6 +38,8 @@ public class Question : MonoBehaviour
         questionlist4();
     }
 
+
+    //add elements into the first dropdown
     void questionlist()
     {
 
@@ -42,40 +47,39 @@ public class Question : MonoBehaviour
 
     }
 
+    //add elements into the second dropdown
     void questionlist2()
     {
 
         dropdown2.AddOptions(questions2);
-
     }
 
+    //add elements in the third dropdown
     void questionlist3()
     {
-
         dropdown3.AddOptions(questions3);
-
     }
 
+    //add elements in the fourth dropdown
     void questionlist4()
     {
-
         dropdown4.AddOptions(questions4); ;
-
     }
 
-
+    //add elements in the fifth dropdown
     void questionlist1()
     {
         dropdown1.AddOptions(questions1);
     }
 
 
-
+    //for question 1, after picking the second option;
     public void DropdownIndex(int index)
     {
         if (index == 1)
         {
             point = 20;
+            GPA = totalpoint1 / 100f;
         }
         else
         {
@@ -83,11 +87,14 @@ public class Question : MonoBehaviour
         }
     }
 
+
+    //for question 2, after picking the third option;
     public void DropdownIndex1(int index)
     {
         if (index == 2)
         {
             point1 = 20;
+            GPA = totalpoint1 / 100f;
         }
         else
         {
@@ -95,11 +102,14 @@ public class Question : MonoBehaviour
         }
     }
 
+
+    //for question 3, after picking the fourth option;
     public void DropdownIndex2(int index)
     {
         if (index == 3)
         {
             point2 = 20;
+            GPA = totalpoint1 / 100f;
         }
         else
         {
@@ -107,11 +117,13 @@ public class Question : MonoBehaviour
         }
     }
 
+    //for question 4, after picking the fourth option;
     public void DropdownIndex3(int index)
     {
         if (index == 3)
         {
             point3 = 20;
+            GPA = totalpoint1 / 100f;
         }
         else
         {
@@ -119,11 +131,14 @@ public class Question : MonoBehaviour
         }
     }
 
+
+    //for question 5, after picking the third option;
     public void DropdownIndex4(int index)
     {
         if (index == 2)
         {
             point4 = 20;
+            GPA = totalpoint1 / 100f;
         }
         else
         {
@@ -131,13 +146,13 @@ public class Question : MonoBehaviour
         }
     }
 
+
+    //total point for the midterm;
     public void total()
 
     {
         totalpoint1 = point + point1 + point2 + point3 + point4;
-        GPA = totalpoint1 / 100f;
         totalpoint.text = totalpoint1.ToString();
-
     }
 
     public void Update()
