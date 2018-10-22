f98e4da97e06924dd743287ec464141b7d056b05
An Huynh <arizhuynh211@gmail.com>
2018-10-21 19:15:01 -0500

    fixed bullet bug

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,11 +6,28 @@ public class Bullet : MonoBehaviour {
     public float speed = 20f;
     public Rigidbody2D temp;
     public int damage = 50;
-
+    private int frame = 0;
+    private bool isShot;
 	// Use this for initialization
 	void Start () {
         temp.velocity = transform.right * speed;
+        isShot = true;
 	}
+
+    void Update()
+    {
+        if (frame < 45)
+        {
+            Debug.Log("Frame: " + frame);
+            frame++;
+        }
+        else
+        {
+            frame = 0;
+            isShot = false;
+            Destroy(gameObject);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
